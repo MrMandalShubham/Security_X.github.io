d3a8e607d91c7268e20cc4f05a5178c75307812b
Mr_Dark <mandalshubham171@gmail.com>
2022-05-20 17:57:28 +0530

    Delete GameOver-main/GameOver directory

--- GameOver-main/GameOver/main.js
+++
@@ -1,8 +0,0 @@
-function GameOver(){
-    setTimeout(() => {        
-        document.getElementById('gameoverEID').play();
-    }, 500);
-    setTimeout(() => {
-        document.getElementById('gameoverSID').play();
-    }, 7500);
-}
